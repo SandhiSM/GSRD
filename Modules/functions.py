32ef77e80c27df0d8486d5109b2bb279c629e2da
SandhiSM <91532714+SandhiSM@users.noreply.github.com>
2022-07-03 09:00:53 +0900

    Added features, improved spelling.

--- Modules/functions.py
+++ Modules/functions.py
@@ -1,1 +1,31 @@
 # これは関数(メソッド)集
+import csv
+import datetime as dt
+import os
+
+
+ENC = 'UTF-8'
+
+
+def error_log(error: str) -> None:
+    time = dt.datetime.now(dt.timedelta(hours=9.0))
+    with open(fr'.\data\crashlogfiles\{time}.txt', 'w', encoding=ENC, newline='') as log_file:
+        log_file.write(error)
+
+
+def csv_load(file_name: str) -> list:
+    with open(fr'.\data\savefiles\{file_name}.csv', encoding=ENC) as csv_file:
+        return [list(value) for value in csv.reader(csv_file)]
+
+
+def csv_write(file_name: str, data: list) -> None:
+    with open(fr'.\data\savefiles\{file_name}.csv', 'w', encoding=ENC, newline='') as csv_file:
+        csv.writer(csv_file).writerows(data)
+
+
+def data_list() -> list:
+    path = r'..\data\savefiles'
+    files = os.listdir(path)
+    data_list = [file_name[:-4]
+                 for file_name in files if os.path.isfile(os.path.join(path, file_name))]
+    return data_list
--- Modules/processor.py
+++ Modules/processor.py
@@ -1,2 +1,91 @@
 # これは内部処理関数集
 import functions
+
+
+def load_data_list() -> list:
+    return functions.data_list()
+
+
+def load_data(file_name: str) -> list:  # データの読み込み
+    try:
+        data = functions.csv_load(file_name)
+    except FileNotFoundError:
+        return []
+    else:
+        return data
+
+
+def register(file_name: str, word: str, meanings: list) -> str:  # 登録
+    if word == '' or meanings == []:
+        return 'You must input not less than one word and one meaning.'
+    else:
+        data: list = load_data(file_name)
+        if find(file_name=file_name, mode='word', findkey=f'{word}') != f'There are no words registered for {word}.':
+            return f'"{word}" has already been registered.'
+        else:
+            newdata = [word]
+            for meaning in meanings:
+                newdata.append(meaning)
+            data.append(word, newdata)
+            return 'Success!'
+
+
+def data_input(file_name: str, new_data_path: str) -> list:  # 一括登録
+    data = load_data(file_name)
+    newdata = functions.csv_load(new_data_path)
+    if newdata == []:
+        return 'There is no such file.'
+    else:
+        for datum in newdata:
+            data.append(datum)
+        functions.csv_write(file_name, data)
+        return 'Success!'
+
+
+def find(file_name: str, mode: str, find_key: str) -> str:  # 確認
+    data: list = load_data(file_name)
+    match mode:
+        case 'word':
+            for datum in data:
+                for meaning in datum[1]:
+                    if meaning == find_key:
+                        return datum[0]
+                    else:
+                        pass
+            else:
+                return f'There are no words registered for {find_key}.'
+        case 'meaning':
+            for datum in data:
+                if datum[0] == find_key:
+                    return datum[1][0], datum[1][1]
+                else:
+                    pass
+            else:
+                return f'There are no meanings registered for {find_key}.'
+        case 'number':
+            return str(len(data)) + 'pcs.'
+
+
+def edit(file_name: str, mode: str, word: str, meanings: list):  # 編集
+    data = load_data(file_name)
+    match mode:
+        case 'word':
+            pass
+        case 'meaning':
+            pass
+
+
+def delete():
+    pass
+
+
+def help(mode: str) -> str:
+    pass
+
+
+def delete_data(file_name: str) -> str:
+    pass
+
+
+def data_output(file_path: str) -> str:
+    pass
