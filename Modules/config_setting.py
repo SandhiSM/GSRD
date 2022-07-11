80115a88a42aba76534871076f2290c0ccc3caa2
S.&'hi.S.M <91532714+SandhiSM@users.noreply.github.com>
2022-07-11 21:24:39 +0900

    Completion of config.

--- Modules/config_setting.py
+++ Modules/config_setting.py
@@ -4,9 +4,10 @@ import configparser
 
 def setting(language="en", password="None", first="False") -> str:
     config = configparser.ConfigParser()
+    config.read(r".\data\config\config.ini")
     config["User"]["language"] = language
-    config["password"] = password
-    config["first"] = first
+    config["User"]["password"] = password
+    config["User"]["first"] = first
     with open(r'.\data\config\config.ini', 'w') as config_file:
         config.write(config_file)
     return "Config file has been successfully edited."
